dcf841f85551d0dbd89d86eb120e97bd12d2e2a3
Kaydan <kaydantran5@gmail.com>
2026-04-21 11:55:02 -0400

    intro scene done

--- Assets/Scripts/ActivePieceController.cs
+++ Assets/Scripts/ActivePieceController.cs
@@ -1353,22 +1353,22 @@ public class ActivePieceController : MonoBehaviour
 
         return new List<IntroDemoPlacement>
         {
-            new IntroDemoPlacement("O_00", "O", new[] { new Vector2Int(0, 0), new Vector2Int(0, 1), new Vector2Int(1, 0), new Vector2Int(1, 1) }),
-            new IntroDemoPlacement("O_01", "O", new[] { new Vector2Int(2, 0), new Vector2Int(2, 1), new Vector2Int(3, 0), new Vector2Int(3, 1) }),
-            new IntroDemoPlacement("T_00", "T", new[] { new Vector2Int(4, 0), new Vector2Int(5, 0), new Vector2Int(5, 1), new Vector2Int(6, 0) }),
-            new IntroDemoPlacement("T_01", "T", new[] { new Vector2Int(6, 1), new Vector2Int(7, 0), new Vector2Int(7, 1), new Vector2Int(7, 2) }),
-            new IntroDemoPlacement("L_00", "L", new[] { new Vector2Int(2, 2), new Vector2Int(3, 2), new Vector2Int(4, 1), new Vector2Int(4, 2) }),
-            new IntroDemoPlacement("L_01", "L", new[] { new Vector2Int(0, 2), new Vector2Int(1, 2), new Vector2Int(1, 3), new Vector2Int(1, 4) }),
-            new IntroDemoPlacement("L_02", "L", new[] { new Vector2Int(3, 3), new Vector2Int(4, 3), new Vector2Int(5, 2), new Vector2Int(5, 3) }),
-            new IntroDemoPlacement("S_00", "S", new[] { new Vector2Int(6, 2), new Vector2Int(6, 3), new Vector2Int(7, 3), new Vector2Int(7, 4) }),
-            new IntroDemoPlacement("I_00", "I", new[] { new Vector2Int(0, 3), new Vector2Int(0, 4), new Vector2Int(0, 5), new Vector2Int(0, 6) }),
-            new IntroDemoPlacement("J_00", "J", new[] { new Vector2Int(1, 5), new Vector2Int(2, 3), new Vector2Int(2, 4), new Vector2Int(2, 5) }),
-            new IntroDemoPlacement("Z_00", "Z", new[] { new Vector2Int(3, 4), new Vector2Int(4, 4), new Vector2Int(4, 5), new Vector2Int(5, 5) }),
-            new IntroDemoPlacement("Z_01", "Z", new[] { new Vector2Int(5, 4), new Vector2Int(6, 4), new Vector2Int(6, 5), new Vector2Int(7, 5) }),
-            new IntroDemoPlacement("J_01", "J", new[] { new Vector2Int(2, 7), new Vector2Int(3, 5), new Vector2Int(3, 6), new Vector2Int(3, 7) }),
-            new IntroDemoPlacement("S_01", "S", new[] { new Vector2Int(0, 7), new Vector2Int(1, 6), new Vector2Int(1, 7), new Vector2Int(2, 6) }),
-            new IntroDemoPlacement("I_01", "I", new[] { new Vector2Int(4, 6), new Vector2Int(5, 6), new Vector2Int(6, 6), new Vector2Int(7, 6) }),
-            new IntroDemoPlacement("I_02", "I", new[] { new Vector2Int(4, 7), new Vector2Int(5, 7), new Vector2Int(6, 7), new Vector2Int(7, 7) })
+            new IntroDemoPlacement("I_00", "I", new[] { new Vector2Int(0, 0), new Vector2Int(1, 0), new Vector2Int(2, 0), new Vector2Int(3, 0) }),
+            new IntroDemoPlacement("J_00", "J", new[] { new Vector2Int(4, 0), new Vector2Int(5, 0), new Vector2Int(6, 0), new Vector2Int(6, 1) }),
+            new IntroDemoPlacement("I_01", "I", new[] { new Vector2Int(7, 0), new Vector2Int(7, 1), new Vector2Int(7, 2), new Vector2Int(7, 3) }),
+            new IntroDemoPlacement("O_00", "O", new[] { new Vector2Int(0, 1), new Vector2Int(0, 2), new Vector2Int(1, 1), new Vector2Int(1, 2) }),
+            new IntroDemoPlacement("L_00", "L", new[] { new Vector2Int(2, 1), new Vector2Int(2, 2), new Vector2Int(3, 1), new Vector2Int(4, 1) }),
+            new IntroDemoPlacement("T_00", "T", new[] { new Vector2Int(5, 1), new Vector2Int(5, 2), new Vector2Int(5, 3), new Vector2Int(6, 2) }),
+            new IntroDemoPlacement("O_01", "O", new[] { new Vector2Int(3, 2), new Vector2Int(3, 3), new Vector2Int(4, 2), new Vector2Int(4, 3) }),
+            new IntroDemoPlacement("L_01", "L", new[] { new Vector2Int(0, 3), new Vector2Int(1, 3), new Vector2Int(1, 4), new Vector2Int(1, 5) }),
+            new IntroDemoPlacement("S_00", "S", new[] { new Vector2Int(2, 3), new Vector2Int(2, 4), new Vector2Int(3, 4), new Vector2Int(3, 5) }),
+            new IntroDemoPlacement("S_01", "S", new[] { new Vector2Int(6, 3), new Vector2Int(6, 4), new Vector2Int(7, 4), new Vector2Int(7, 5) }),
+            new IntroDemoPlacement("I_02", "I", new[] { new Vector2Int(0, 4), new Vector2Int(0, 5), new Vector2Int(0, 6), new Vector2Int(0, 7) }),
+            new IntroDemoPlacement("Z_00", "Z", new[] { new Vector2Int(4, 4), new Vector2Int(5, 4), new Vector2Int(5, 5), new Vector2Int(6, 5) }),
+            new IntroDemoPlacement("Z_01", "Z", new[] { new Vector2Int(1, 6), new Vector2Int(1, 7), new Vector2Int(2, 5), new Vector2Int(2, 6) }),
+            new IntroDemoPlacement("J_01", "J", new[] { new Vector2Int(4, 5), new Vector2Int(4, 6), new Vector2Int(5, 6), new Vector2Int(6, 6) }),
+            new IntroDemoPlacement("T_01", "T", new[] { new Vector2Int(2, 7), new Vector2Int(3, 6), new Vector2Int(3, 7), new Vector2Int(4, 7) }),
+            new IntroDemoPlacement("L_02", "L", new[] { new Vector2Int(5, 7), new Vector2Int(6, 7), new Vector2Int(7, 6), new Vector2Int(7, 7) })
         };
     }
 
